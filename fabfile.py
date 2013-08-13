243b472969f7cd346f5f0fe63b6fbdadf9d87660
Raony Guimarães <raonyguimaraes@gmail.com>
2013-08-13 17:07:15 -0300

    changes

--- fabfile.py
+++ fabfile.py
@@ -223,4 +223,4 @@ def deploy_medfuturo():
 
     with cd('/home/raony/webapps/medfuturo/medfuturo/'):
         run('git pull')
-        run('../apache/bin/restart')
+        run('sh ../apache/bin/restart')
